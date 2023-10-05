4b93d7b983caedd4bb3dc8800f8876cbe7e81ce3
Arron-B <arronmail@yahoo.com>
2023-10-05 20:00:30 +0100

    11 all articles topic query ready for review

--- app.js
+++ app.js
@@ -15,8 +15,11 @@ app.get('/api/articles', getAllArticles);
 app.get('/api/articles/:article_id', getArticleById);
 
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
+
 app.post('/api/articles/:article_id/comments', addComment);
+
 app.patch('/api/articles/:article_id', patchArticle);
+
 
 
 app.use(handleCustomErrors);
--- models/get-models.js
+++ models/get-models.js
@@ -25,14 +25,41 @@ exports.fetchArticleById = (article_id) => {
     })
 }
 
-exports.fetchAllArticles = () => {
-    return db.query(
-        `SELECT a.article_id, a.author, a.title, a.topic, a.created_at, a.votes, a.article_img_url, CAST(COUNT(c.article_id) AS INT) comment_count
+exports.fetchAllArticles = (topic) => {
+    return db.query(`
+        SELECT * FROM topics
+        WHERE slug = $1;`, [topic]).then((res) => {
+        if(res.rowCount === 0 && topic) {
+            return Promise.reject({
+                status: 404,
+                msg: 'Topic does not exist'
+              })
+            }
+    
+    let query = `
+        SELECT a.article_id, a.author, a.title, a.topic, a.created_at, a.votes, a.article_img_url, CAST(COUNT(c.article_id) AS INT) comment_count
         FROM articles a 
         LEFT JOIN comments c 
-        ON a.article_id = c.article_id
-        GROUP BY a.article_id
-        ORDER BY a.created_at DESC;`).then((res) => {
+        ON a.article_id = c.article_id`;
+        if(topic) {
+            query += `  WHERE a.topic = $1
+                        GROUP BY a.article_id
+                        ORDER BY a.created_at DESC;`;
+            return db.query(query, [topic])
+        }
+    
+        query += ` GROUP BY a.article_id
+                  ORDER BY a.created_at DESC;`;
+        
+        return db.query(query)
+    })
+        .then((res) => {
+        if(res.rowCount === 0) {
+            return Promise.reject({
+                status: 404,
+                msg: 'There are no articles of this topic'
+                })
+            }
         const allArticles = res.rows
         return allArticles;
     })
