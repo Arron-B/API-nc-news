ac8b96abaf08f1f36b6ddfe33d2b96cc8b634600
Arron-B <arronmail@yahoo.com>
2023-10-23 11:04:30 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const express = require("express");
-const {getTopics, getEndPoints, getAllArticles, getArticleById, getCommentsByArticleId, addComment, patchArticle, getAllUsers, deleteComment} = require('./controllers/index.js')
-const {handleCustomErrors, handlePsqlErrors, handle500Errors} = require('./error-handlers/errors.index.js')
+const {
+	getTopics,
+	getEndPoints,
+	getAllArticles,
+	getArticleById,
+	getCommentsByArticleId,
+	addComment,
+	patchArticle,
+	getAllUsers,
+	deleteComment,
+} = require("./controllers/index.js");
+const {
+	handleCustomErrors,
+	handlePsqlErrors,
+	handle500Errors,
+} = require("./error-handlers/errors.index.js");
+const cors = require("cors");
 
 app = express();
 
-app.use(express.json())
+app.use(cors());
 
-app.get('/api/topics', getTopics);
+app.use(express.json());
 
-app.get('/api', getEndPoints);
+app.get("/api/topics", getTopics);
 
-app.get('/api/articles', getAllArticles);
+app.get("/api", getEndPoints);
 
-app.get('/api/articles/:article_id', getArticleById);
+app.get("/api/articles", getAllArticles);
 
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
+app.get("/api/articles/:article_id", getArticleById);
 
-app.post('/api/articles/:article_id/comments', addComment);
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
-app.patch('/api/articles/:article_id', patchArticle);
+app.post("/api/articles/:article_id/comments", addComment);
 
-app.delete('/api/comments/:comment_id', deleteComment);
+app.patch("/api/articles/:article_id", patchArticle);
 
-app.get('/api/users', getAllUsers)
+app.delete("/api/comments/:comment_id", deleteComment);
+
+app.get("/api/users", getAllUsers);
 
 app.use(handleCustomErrors);
 
